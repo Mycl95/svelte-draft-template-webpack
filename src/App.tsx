5021775ff6fec7166b901849f792b4646de45589
Mycl95 <fabian.michael95@gmail.com>
2020-03-20 19:16:02 +0100

    update welcome message in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,10 @@ export default function App(props: IAppProps)
 
     <main>
         <h1>Hello {name}!</h1>
-        <p>Visit the <Link url="https://svelte.dev/tutorial" text="Svelte tutorial" /> to learn how to build Svelte apps.</p>
+        <p>
+            Visit the <Link url="https://svelte.dev/tutorial" text="Svelte tutorial" /> to learn how to build Svelte apps and the <Link url={"https://github.com/mistlog/svelte-draft-docs"} text={"svelte-draft Docs"}/> to learn how to do it in <Link url={"https://github.com/mistlog/svelte-draft"} text={"svelte-draft"}/>.
+        </p>
+        <p>A complete svelte-draft version of the tutorial is also available at <Link url={"https://github.com/mistlog/svelte-draft-tutorial"} text={"mistlog/svelte-draft-tutorial"}/> on GitHub</p>
+        <p></p>
     </main>
 }
